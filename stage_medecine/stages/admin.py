b3b68b5a167a46a41a761fdaf8c31e2bcf3197fa
Louis Guillou <louis.guillou98@gmail.com>
2021-02-17 23:54:05 +0100

    Added all class to admin

--- stage_medecine/stages/admin.py
+++ stage_medecine/stages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from stages.models import StageDone, Stage, Category, Period
+
+admin.site.register(StageDone)
+admin.site.register(Stage)
+admin.site.register(Category)
+admin.site.register(Period)
